54eaeb333d86ca1cf7d87e4c3fe21389e0ab1a76
Adam Kentosh <akentosh@gmail.com>
2021-01-26 16:51:27 -0500

    s/wait/sleep

--- setup_vault.sh
+++ setup_vault.sh
@@ -3,5 +3,5 @@ set -e
 
 ## Vault Config
 nohup vault server -dev -dev-root-token-id=root &>/tmp/vault.log &&
-wait 10 &&
-vault write sys/license $(cat ~/.ssh/vault.hclic)
+sleep 5 &&
+vault write sys/license "$(cat ~/.ssh/vault.hclic)"
